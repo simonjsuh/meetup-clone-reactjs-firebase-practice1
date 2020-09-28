26fd74accdb6296906730c3861afbaed7782ee4f
simonjsuh <simon.j.suh@gmail.com>
2020-09-27 18:33:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import '../node_modules/firebaseui/dist/firebaseui.css';
 import { firebaseDB, firebaseUI } from './firebase';
 
 function App() {
+  // let user = firebaseDB.auth().currentUser;
+
+  // if (user) {
+  //   alert('user ' + user + ' is signed in');
+  // } else {
+    
+  // }
+
+  const [signedInUser, setSignedInUser] = useState('');
+
   firebaseUI.start('#firebaseui-auth-container', {
-    signInSuccessUrl: 'http://www.simonjsuh.com',
+    signInSuccessUrl: 'http://localhost:3000',
     signInOptions: [
       // List of OAuth providers supported.
       firebaseDB.firebase_.auth.EmailAuthProvider.PROVIDER_ID,
@@ -14,6 +24,26 @@ function App() {
     ],
     // Other config options...
   });
+
+  firebaseDB.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      alert('user ' + user.displayName + ' is signed in');
+      document.getElementById('firebaseui-auth-container').style.display = 'none';
+      setSignedInUser(user.displayName);
+    } else {
+      
+    }
+  });
+
+  // Firebase OAuth signout function
+  function signOut(e) {
+    e.preventDefault();
+    firebaseDB.auth().signOut().then(() => {
+      alert('no users signed in');
+    }, err => {
+      console.error('Sign Out Error', err);
+    });
+  }
 
   // // firebase OAuth Google Sign-in Pop-up form code
   // firebaseDB.auth().signInWithPopup(firebaseGoogleProvider).then(function(result) {
@@ -36,6 +66,12 @@ function App() {
   return (
     <div className="App">
       <div id="firebaseui-auth-container"></div>
+      <div id="welcomeMsg">
+        <h1>Welcome {signedInUser}</h1>
+      </div>
+      <div id="signOutBtn">
+        <a href='' onClick={signOut}>Sign Out</a>
+      </div>
     </div>
   );
 }

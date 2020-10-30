1c7d2a44a943e93f85c2f6f6f0d2d0d155f01ac0
simonjsuh <simon.j.suh@gmail.com>
2020-10-30 12:53:02 -0700

    tag input placeholder conditionally programmed in

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -1,12 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './CreateGroup.css';
 
 // import Google Maps component
 import GoogleMap from './GoogleMap';
 
+// import Google Map Places Autocomplete Input field you created
 import GoogleMapPlacesAutoCompleteInputField from './GoogleMapPlacesAutoCompleteInputField';
 
+// import Google materials ui icon
+import CloseIcon from '@material-ui/icons/Close';
+
 export default function CreateGroup() {
+  const [tags, setTags] = useState([]);
+  // const [tagsArrayEmpty, setTagsArrayEmpty] = useState(true);
+
+  const formTagContainer = useRef(null);
+
+  // formTagContainer.addEventListener('keydown', (event) => {
+  //   alert('hisdf');
+  // });
+
+  function formPreventDefault(e) {
+    // formTagContainer.current.appendChild(createTag('Hypnosis'));
+    e.preventDefault();
+  }
+
+  function addTag(e) {
+    if (e.keyCode == 13) {
+      console.log('label is: ' + e.target.value);
+      setTags([...tags, { label: e.target.value }]);
+      e.target.value = '';
+    }
+  }
+  
+  useEffect(() => {
+    // setTags([...tags, {label: 'Jumbotron'}, {label: 'CSS'}]);
+    // addTag('hi');
+  }, []);
+
   return (
     <div id='createGroupForm'>
       <div className="left">
@@ -14,14 +45,13 @@ export default function CreateGroup() {
       </div>
       <div className="right py-5 px-3">
         <h1 className='text-center'>Create a Group</h1>
-        <form className='my-4'>
+        <form className='my-4' onSubmit={formPreventDefault}>
           <div className="form-group">
             <label htmlFor="groupName">Group name</label>
             <input type="text" id='groupName' className='form-control' placeholder='Enter group name' />
           </div>
           <div className="form-group">
             <label htmlFor="groupAddress">Address</label>
-            {/* <input type="text" id='groupAddress' className="form-control" placeholder='Enter group address' /> */}
             <GoogleMapPlacesAutoCompleteInputField />
           </div>
           <div className="form-group">
@@ -30,7 +60,26 @@ export default function CreateGroup() {
           </div>
           <div className="form-group">
             <label htmlFor="groupTags">Group tags (for searching groups)</label>
-            <textarea name="grouptagstextarea" id="groupTags" className='form-control' placeholder='Enter tags separated by a comma'></textarea>
+            
+            {/* tag creation area of form */}
+            <div ref={formTagContainer} className="tagContainer">
+              {tags.map(tag => {
+                return <div className='tag'>{tag.label}<CloseIcon /></div>
+              })}
+              {
+                (tags.length == 0) ?
+                  <input
+                    onKeyUp={addTag} 
+                    placeholder='Type your tags separated by enter key'
+                    type='text'
+                  />
+                  :
+                  <input
+                    onKeyUp={addTag} 
+                    type='text'
+                  />
+              }
+            </div>
           </div>
           <button className="btn btn-primary form-control">Create</button>
         </form>
